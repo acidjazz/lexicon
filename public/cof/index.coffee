d8a675832ee769b966378b4b2b31ce718eb19fcf
kevin olson <acidjazz@gmail.com>
2015-05-27 02:43:41 -0700

    admin v0.1

--- public/cof/index.coffee
+++ public/cof/index.coffee
@@ -1,7 +1,7 @@
 Index =
 
   i: ->
-    console.log 'Index.i()'
+
     Waves.attach '.button', ['waves-float', 'waves-light']
     Waves.init()
     
--- public/jst/index.js
+++ public/jst/index.js
@@ -2,7 +2,6 @@ var Index;
 
 Index = {
   i: function() {
-    console.log('Index.i()');
     Waves.attach('.button', ['waves-float', 'waves-light']);
     return Waves.init();
   }
