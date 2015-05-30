b560175c0e72b9a7624d858740857fd596181614
kevin olson <acidjazz@gmail.com>
2015-05-29 20:19:13 -0700

    restructuring

--- site/pub/index.php
+++ site/pub/index.php
@@ -1,12 +1,14 @@
 <?
 
-$loader = require '../vendor/autoload.php';
-$loader->setPsr4('ctl\\', 'ctl/');
+$loader = require '../../vendor/autoload.php';
+$loader->setPsr4('ctl\\', '../ctl/');
 
 set_error_handler(['\tryg\Debug', 'handler'], E_ALL);
 register_shutdown_function(['\tryg\Debug', 'shutdown']);
 
-tryg\Debug::def(json_decode(file_get_contents(__DIR__.'/cfg/config.json'), true)['cfg']);
+tryg\Debug::def(json_decode(file_get_contents(__DIR__.'/../cfg/config.json'), true)['cfg']);
+date_default_timezone_set($cfg['tryg']['timezone']);
+
 function hpr() { return call_user_func_array(['\tryg\Debug', 'hpr'], func_get_args()); }
 
 (new tryg\Controller($_SERVER['REQUEST_URI']))->start();
